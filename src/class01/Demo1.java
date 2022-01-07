7f6724c6a439bf181db887bc4896fcc832241ec7
angelicazaporozhets <angiezap95@gmail.com>
2022-01-07 12:32:32 -0600

    first class and HW

---
+++ src/class01/Demo1.java
@@ -0,0 +1,24 @@
+package class01;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+    @Test
+    public void firstTest(){
+
+            System.out.println("I am a first test");
+
+        }
+        @Test
+        public void secondTest(){
+            System.out.println("I am a second test");
+        }
+        @Test
+        public void thirdTest(){
+            System.out.println("I am third test");
+        }
+
+
+    }
+
---
+++ src/class01/HomeWork.java
@@ -0,0 +1,29 @@
+package class01;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomeWork {
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Pre condition");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("Post condition");
+    }
+
+    @Test(priority = 1)
+    public void openBrowser(){
+        System.out.println("Opening the browser");
+    }
+    @Test(priority=2)
+    public void login(){
+        System.out.println("Log in with valid credentials");
+    }
+    @Test(priority=3)
+    public void tearDown(){
+        System.out.println("Closing the browser");
+    }
+}
